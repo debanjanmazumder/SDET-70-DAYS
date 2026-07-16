d02c16755d9e0f3e1b00c2a27543249e41ca43bd
Debanjan Mazumder <debanjanmazumder070@gmail.com>
2026-07-16 21:13:44 +0530

    Add program to calculate total bugs in an array

---
+++ day3/array.java
@@ -0,0 +1,17 @@
+package day3;
+
+public class day3_array {
+
+	public static void main(String[] args) {
+	
+		        System.out.println("--- PROGRAM 1: TOTAL BUGS ---");
+		        int[] bugs = {5, 12, 3, 8};
+		        int total = 0;
+		        for(int i = 0; i < bugs.length; i++) {
+		            total = total + bugs[i];
+		        }
+		        System.out.println("Total Bugs: " + total); 
+
+		        
+		        }
+}
